17c511fdc3dfd59a8df269dd55a2e924ae9abf28
Michael Yeboah Frimpong <46923296+mickeydev@users.noreply.github.com>
2022-11-09 12:13:42 +0530

    calendar_Sidebar_fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
-// import Calendar from './scenes/calendar';
+import Calendar from './scenes/calendar';
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
@@ -16,34 +16,34 @@ import Form from './scenes/form';
 // import Geography from './scenes/geography';
 
 function App() {
-  const [theme, colorMode] = useMode();
+   const [theme, colorMode] = useMode();
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidebar />
-          <main className="content">
-            <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
-              {/* <Route path='/faq' element={<FAQ/>} /> */}
-              {/* <Route path='/bar' element={<Bar />} /> */}
-              {/* <Route path='/pie' element={<Pie />} /> */}
-              {/* <Route path='/line' element={<Line />} /> */}
-              {/* <Route path='/geography' element={<Geography />} /> */}
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+   return (
+      <ColorModeContext.Provider value={colorMode}>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+               <Sidebar />
+               <main className="content">
+                  <Topbar />
+                  <Routes>
+                     <Route path="/" element={<Dashboard />} />
+                     <Route path="/team" element={<Team />} />
+                     <Route path="/contacts" element={<Contacts />} />
+                     <Route path="/invoices" element={<Invoices />} />
+                     <Route path="/form" element={<Form />} />
+                     <Route path="/calendar" element={<Calendar />} />
+                     {/* <Route path='/faq' element={<FAQ/>} /> */}
+                     {/* <Route path='/bar' element={<Bar />} /> */}
+                     {/* <Route path='/pie' element={<Pie />} /> */}
+                     {/* <Route path='/line' element={<Line />} /> */}
+                     {/* <Route path='/geography' element={<Geography />} /> */}
+                  </Routes>
+               </main>
+            </div>
+         </ThemeProvider>
+      </ColorModeContext.Provider>
+   );
 }
 
 export default App;
